e83c139928744d36707bc6d1defce7e32741d4d4
Andrey Mekeda <blestan5@gmail.com>
2022-09-28 19:46:56 +0300

    Зробив меню без перегрузки сторінки

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 const API = '1ca1726301773f02a2fe357a72b037cd';
 const URL = 'https://api.openweathermap.org/data/3.0/onecall?lat=';
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+$('#today').click(function() {
+    $.ajax({
+        url: 'today.html',
+        dataType: 'html'
+    }).done(function(data) {
+        $('#content').empty();
+        $('#content').html(data);
+    }).fail(function() {
+        $('#content').empty();
+        $('#content').html('<h1>404 Error</h1>');
+    });
+});
+
+$('#five_day').click(function() {
+    $.ajax({
+        url: 'five-day.html',
+        dataType: 'html'
+    }).done(function(data) {
+        $('#content').empty();
+        $('#content').html(data);
+    }).fail(function() {
+        $('#content').empty();
+        $('#content').html('<h1>404 Error</h1>');
+    });
+});
