4435b0cb93db71115f79b5fe6f7324252e89cdae
Andrey Mekeda <blestan5@gmail.com>
2022-09-28 20:45:21 +0300

    Додав фавікон до сайту

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const API = '1ca1726301773f02a2fe357a72b037cd';
 const URL = 'https://api.openweathermap.org/data/3.0/onecall?lat=';
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+const exampl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=';
+
 
 $().ready(function() {
     $.ajax({
@@ -39,3 +41,13 @@ $('#five_day').click(function() {
         $('#content').html('<h1>404 Error</h1>');
     });
 });
+
+$.ajax({
+    url: exampl + API,
+    dataType: "json"
+}).done(function(data) {
+    console.log(data);
+}).fail(function() {
+    console.log("Error json")
+});
+// console.log(exampl + API);
