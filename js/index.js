d4a28982d1cdb60b384144c16db82e7fed827212
Andrey Mekeda <blestan5@gmail.com>
2022-09-28 20:20:09 +0300

    Зробив щоб на index.html автоматом завантажувалася сторінка today.html

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ const API = '1ca1726301773f02a2fe357a72b037cd';
 const URL = 'https://api.openweathermap.org/data/3.0/onecall?lat=';
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
+$().ready(function() {
+    $.ajax({
+        url: 'today.html',
+        dataType: 'html'
+    }).done(function(data) {
+        $('#content').empty();
+        $('#content').html(data);
+    }).fail(function() {
+        $('#content').empty();
+        $('#content').html('<h1>404 Error</h1>');
+    });
+})
 $('#today').click(function() {
     $.ajax({
         url: 'today.html',
